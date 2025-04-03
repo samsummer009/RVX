107aad3fed7c3a0554ff3ae41c70239a83a064f3
Obasu <samneill852@gmail.com>
2025-04-03 23:23:55 +0500

    Initial commit

---
+++ run-revanced-builder.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Running ReVanced Builder through WSL...
+wsl bash -c "cd /mnt/c/Users/Obasu/Desktop/Revanced/revanced-builder-52 && ./build.sh"
+echo.
+echo Build process completed.
+pause
